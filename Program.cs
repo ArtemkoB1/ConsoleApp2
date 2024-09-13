35fa61eb468f3a1204d62369e8b7975cd2ce79fc
Admin <Admin@DESKTOP-N7GTE0T>
2024-09-13 20:36:18 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         string name = "my name Artem Bilenkyi";
+          int age = 17;
+           Console.WriteLine("Helo,{0},i'm {1} years old!", name, age);
+           Console.ReadKey();
+        
+        }
+}
+    }
